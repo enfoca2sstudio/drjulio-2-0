785c46870b76f0e1c006ba27a98c6abd873d4cc6
Fabio <fabiochdev@gmail.com>
2026-05-20 19:34:03 -0400

    web nueva drjulio 1.0.15

---
+++ js/index.js
@@ -0,0 +1,24 @@
+(function ($) {
+  "use strict";
+
+  $(".procedi-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+})(jQuery);
