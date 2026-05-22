86bc2cbec2617e3a524007311fc1a4fe0e952fb6
Fabio <fabiochdev@gmail.com>
2026-05-22 19:31:31 -0400

    web drjulio 1.0.4

--- js/index.js
+++ js/index.js
@@ -21,4 +21,25 @@
       },
     },
   });
+
+  $(".somos-hero-carousel").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      992: {
+        items: 1,
+      },
+    },
+  });
 })(jQuery);
