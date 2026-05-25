c4beb9142a16fd01c82c366a534942fa14c999da
Fabio <masterhollowinfinite@gmail.com>
2026-05-25 19:57:07 -0400

    web nueva drjulio 1.1

--- js/index.js
+++ js/index.js
@@ -42,4 +42,32 @@
       },
     },
   });
+
+  // procedimientos
+  // 1. Abrir el primero por defecto al cargar
+  const primerDropdown = document.querySelector(".btn-procedi");
+  if (primerDropdown) {
+    const bsDropdown = new bootstrap.Dropdown(primerDropdown);
+    bsDropdown.show();
+  }
+
+  // 2. Control de scroll modificado para respetar el contenedor padre
+  const botones = document.querySelectorAll(".btn-procedi");
+
+  botones.forEach(function (boton) {
+    boton.addEventListener("click", function (e) {
+      // En lugar del botón, buscamos el contenedor padre completo
+      const contenedorPadre = this.closest(".item-procedis");
+
+      if (contenedorPadre) {
+        requestAnimationFrame(function () {
+          // Hacemos el scroll al contenedor, el cual obedecerá al 'scroll-margin-top' de CSS
+          contenedorPadre.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+          });
+        });
+      }
+    });
+  });
 })(jQuery);
